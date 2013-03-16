24dcf0c4a4dc1deaf00ff87aefda71fa5164fcac
wangli <wangli3@ucweb.com>
2013-03-16 16:02:16 +0800

    add makefile project

---
+++ src/a.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    std::cout << "test buildbot src cpp file!" << std::endl;
+    std::cout << "add a line!" << std::endl;
+    return 0;
+}
