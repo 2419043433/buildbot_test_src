9002f191eb7b397299299eb693f04964e42024b4
wangli <wangli3@ucweb.com>
2013-03-18 10:52:42 +0800

    add error to test email

--- a.cpp
+++ a.cpp
@@ -4,7 +4,8 @@ using namespace std;
 
 int main()
 {
-    std::cout << "test buildbot src cpp file!" << std::endl;
+    std::cout1 << "test buildbot src cpp file!" << std::endl;
+    std::cout << "add email notifier!" << std::endl;
     std::cout << "add a line!" << std::endl;
     return 0;
 }
